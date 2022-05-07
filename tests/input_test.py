5d7d1c0bc87c6fc5ba82a1af46579ca37e2498f8
lizaveta56k <lizaveta.kaliaha@gmail.com>
2022-05-07 16:44:12 +0200

    updated tests

--- tests/input_test.py
+++ tests/input_test.py
@@ -23,3 +23,45 @@ def test_error_for_invalid_test_split_ratio(
     )
     assert result.exit_code == 2
     assert "Invalid value for '--test-split-ratio'" in result.output
+
+def test_error_for_invalid_max_iter(
+    runner: CliRunner
+) -> None:
+    """It fails when max iter is lower than 1."""
+    result = runner.invoke(
+        train,
+        [
+            "--max-iter",
+            0,
+        ],
+    )
+    assert result.exit_code == 2
+    assert "Invalid value for '--max-iter'" in result.output
+
+def test_error_for_invalid_n_init(
+    runner: CliRunner
+) -> None:
+    """It fails when n init is lower than 1."""
+    result = runner.invoke(
+        train,
+        [
+            "--n_init",
+            0,
+        ],
+    )
+    assert result.exit_code == 2
+    assert "Invalid value for '--n_init'" in result.output
+
+def test_error_for_invalid_n_clusters(
+    runner: CliRunner
+) -> None:
+    """It fails when n clusters is lower than 1."""
+    result = runner.invoke(
+        train,
+        [
+            "--n_clusters",
+            0,
+        ],
+    )
+    assert result.exit_code == 2
+    assert "Invalid value for '--n_clusters'" in result.output
