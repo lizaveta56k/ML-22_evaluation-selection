fec8830ff904d047d167214941935fe148113c06
lizaveta56k <lizaveta.kaliaha@gmail.com>
2022-05-07 16:21:37 +0200

    added train flow

---
+++ src/poetry_task_9/train.py
@@ -0,0 +1,90 @@
+from pathlib import Path
+from joblib import dump
+
+import click
+import mlflow
+import mlflow.sklearn
+from sklearn.metrics import accuracy_score
+
+from .data import get_dataset
+from .pipeline import create_pipeline
+
+
+@click.command()
+@click.option(
+    "-d",
+    "--dataset-path",
+    default="data/train.csv",
+    type=click.Path(exists=True, dir_okay=False, path_type=Path),
+    show_default=True,
+)
+@click.option(
+    "-s",
+    "--save-model-path",
+    default="data/model.joblib",
+    type=click.Path(dir_okay=False, writable=True, path_type=Path),
+    show_default=True,
+)
+@click.option(
+    "--random-state",
+    default=42,
+    type=int,
+    show_default=True,
+)
+@click.option(
+    "--test-split-ratio",
+    default=0.2,
+    type=click.FloatRange(0, 1, min_open=True, max_open=True),
+    show_default=True,
+)
+@click.option(
+    "--use-scaler",
+    default=True,
+    type=bool,
+    show_default=True,
+)
+@click.option(
+    "--max-iter",
+    default=100,
+    type=int,
+    show_default=True,
+)
+@click.option(
+    "--n_init",
+    default=10,
+    type=int,
+    show_default=True,
+)
+@click.option(
+    "--n_clusters",
+    default=8,
+    type=int,
+    show_default=True,
+)
+def train(
+    dataset_path: Path,
+    save_model_path: Path,
+    random_state: int,
+    test_split_ratio: float,
+    use_scaler: bool,
+    max_iter: int,
+    n_init: int,
+    n_clusters: int
+) -> None:
+    features_train, features_val, target_train, target_val = get_dataset(
+        dataset_path,
+        random_state,
+        test_split_ratio,
+    )
+    with mlflow.start_run():
+        pipeline = create_pipeline(use_scaler, n_clusters, max_iter, n_init, random_state)
+        pipeline.fit(features_train, target_train)
+        accuracy = accuracy_score(target_val, pipeline.predict(features_val))
+        mlflow.log_param("use_scaler", use_scaler)
+        mlflow.log_param("n_clusters", n_clusters)
+        mlflow.log_param("max_iter", max_iter)
+        mlflow.log_param("n_init", n_init)
+        mlflow.log_metric("accuracy", accuracy)
+        click.echo(f"Accuracy: {accuracy}.")
+        dump(pipeline, save_model_path)
+        click.echo(f"Model is saved to {save_model_path}.")
